dc1cc734a260841d55ece869320599868af94b2a
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-03-10 04:23:55 +0600

    Page Structure changed
    Signed-off-by: Khan Asfi Reza <khanasfireza10@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Navbar from "../components/Navigation";
+import Hero from "../elements/Home/Hero";
+import About from "../elements/Home/About";
 
 export default function Home() {
-  return (
-      <>
-      </>
-  )
+    return (
+        <div className={""}>
+            <Navbar/>
+            <main className={"scroll-smooth perspective h-screen overflow-x-hidden overflow-y-auto bg-black"}>
+                <Hero/>
+                <About/>
+            </main>
+        </div>
+    )
 }
