c20a61d6e7a8c8887f58cb7f8a78bfb765e313a7
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-03-10 04:23:45 +0600

    Config File Fix
    Signed-off-by: Khan Asfi Reza <khanasfireza10@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,34 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
     content: [
+        "./assets/**/*.{js,ts,jsx,tsx}",
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
+        "./elements/**/*.{js,ts,jsx,tsx}",
     ],
+
     theme: {
-        extend: {},
+        screens: {
+            'sxs': '345px',
+            'xs': '475px',
+            ...defaultTheme.screens,
+        },
+        extend: {
+            colors: {
+                primary: "#1e3cb7",
+                secondary: "#8eee9b",
+                clrHeading: "#111827",
+                clrsSubheading: "",
+                clrText: "#464749",
+                clrWhite: "#fff",
+                clrGray: "#e9e7e7",
+                bgDark: "#f4f3f5",
+                bgLight: "#f7f7f7",
+                darkBlue: "#0E1134",
+                gradientBlue: "#0f054c",
+                gradientLightBlue: "#5761d7"
+            }
+        },
     },
     plugins: [],
 }
