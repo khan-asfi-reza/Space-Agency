bfcb5d1b752c7854286ae0068f9859382e303732
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-03-12 03:08:07 +0600

    About Style Changes
    Signed-off-by: Khan Asfi Reza <khanasfireza10@gmail.com>

--- elements/Home/About.js
+++ elements/Home/About.js
@@ -1,20 +1,38 @@
-import Juptiter from "../../assets/images/jupiter.png"
+import Jupiter from "../../assets/images/jupiter.png"
 import Mars from "../../assets/images/BlueNew.png"
 import Earth from "../../assets/images/BluePlanet.png"
 import Sky from "../../assets/images/sky1.jpg"
 import Image from "next/image";
 import {FaStar} from "react-icons/fa";
+import {motion} from "framer-motion";
+import {PLANET_ID} from "../../assets/data";
 
 function range(i) {
     return [...Array(i).keys()];
 }
 
 
-const PlanetCard = ({image, title, text, star, price}) => (
-    <div className={"h-[28rem] relative"}>
-        <div className="absolute inset-0 overflow-hidden">
+const PlanetCard = ({image, title, text, star, price, delay}) => (
+    <motion.div
+        initial={{opacity: 0, y: 10}}
+        whileInView={{opacity: 1, y: 0}}
+        viewport={{once: true}}
+        transition={{duration: 0.4, delay: 0.1 * delay}}
+        className={"h-[28rem] relative"}>
+        <motion.div
+            animate={{
+                y: [15, 0, 15]
+            }}
+            transition={{
+                duration: 3,
+                ease: "easeInOut",
+                repeat: Infinity,
+                repeatDelay: 0.5,
+                delay: 0.5 * delay
+            }}
+            className="absolute inset-0 overflow-hidden">
             <Image alt={title} src={image} layout={"fill"} objectFit={"contain"}/>
-        </div>
+        </motion.div>
         <div className={"lg:w-10/12 w-full mx-auto absolute left-1/2 transform top-1/2 -translate-x-1/2"}>
             <div className={"glass lg:p-10 md:p-8 p-6 w-full"}>
                 <h3 className={"text-white font-medium lg:text-2xl text-xl"}>
@@ -35,29 +53,37 @@ const PlanetCard = ({image, title, text, star, price}) => (
                 </p>
             </div>
         </div>
-    </div>
+    </motion.div>
 )
 
 
 export default function About() {
     return (
-        <section style={{backgroundImage: `url(${Sky.src})`}} className={"bg-cover relative min-h-screen z-20"}>
+        <section
+            id={PLANET_ID}
+            style={{backgroundImage: `url(${Sky.src})`}} className={"bg-cover relative min-h-screen z-20"}>
             <div className={"absolute z-10 top-0 w-full h-32 bg-gradient-to-b from-black to-transparent"}>
 
             </div>
-            <section className={"z-20 container mx-auto px-4 py-16"}>
-                <h2 className={"text-6xl font-medium text-white text-center"}>
+            <section className={"z-20 container mx-auto px-4 py-20"}>
+                <motion.h2
+                    initial={{opacity: 0, x: -100}}
+                    whileInView={{opacity: 1, x: 0}}
+                    viewport={{once: true}}
+                    transition={{duration: 0.4, delay: 0.1}}
+                    className={"text-6xl font-medium text-white text-center"}>
                     Our Planetary Packages
-                </h2>
+                </motion.h2>
                 <p className={"text-lg text-gray-300 text-center mt-4"}>
                     Get the best Planet Package within your price range
                 </p>
                 <div className="grid lg:grid-cols-3 lg:gap-20 gap-4 xs:grid-cols-2 mt-32">
-                    <PlanetCard image={Juptiter}
+                    <PlanetCard image={Jupiter}
                                 title={"Jupiter"}
                                 text={"The most expensive and luxurious tour ever"}
                                 star={5}
                                 price={"12,000"}
+                                delay={0}
                     />
 
                     <PlanetCard image={Mars}
@@ -65,6 +91,7 @@ export default function About() {
                                 text={"The most satisfying and luxurious tour ever"}
                                 star={5}
                                 price={"12,000"}
+                                delay={1}
                     />
 
                     <PlanetCard image={Earth}
@@ -72,6 +99,7 @@ export default function About() {
                                 text={"The most coolest and luxurious tour ever"}
                                 star={5}
                                 price={"12,000"}
+                                delay={2}
                     />
                 </div>
             </section>
