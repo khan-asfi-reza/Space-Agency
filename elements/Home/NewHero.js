4a480ad512fdce92f6d7eecaa30aaf93bb67d7f8
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-03-12 03:10:44 +0600

    Hero style change
    Signed-off-by: Khan Asfi Reza <khanasfireza10@gmail.com>

--- elements/Home/NewHero.js
+++ elements/Home/NewHero.js
@@ -1,10 +1,12 @@
 import {FaArrowRight, FaMoneyBill, FaRocket, FaStar} from "react-icons/fa";
 import {motion} from "framer-motion";
+import Link from "next/link";
+import {BOOK_ID, HERO_ID} from "../../assets/data";
 
 export function NewHero() {
 
     return (
-        <section className={"relative min-h-screen w-full "}>
+        <section id={HERO_ID} className={"relative min-h-screen w-full "}>
             <video preload={"true"} muted={true} className={"z-1 absolute object-cover h-full w-full"} autoPlay={true}
                    loop={true}>
                 <source src={"/bgvideo.mp4"}/>
@@ -22,17 +24,19 @@ export function NewHero() {
                     <p className={"text-center font-light text-gray-200 mt-12 text-xl"}>
                         We provide the best service to go to the outer space, join us for more thrilling adventure
                     </p>
-                    <a className={"relative mt-12 mx-auto inline-block transition-all duration-300 group hover:bg-white hover:text-gray-900 overflow-visible xl:px-16 lg:px-14 lg:py-4 md:px-12 md:py-4 px-10 py-3 xl:py-6 text-xl text-white bg-transparent border border-white"}>
-                        Book Now
-                        <span
-                            className={"bg-white grid transition-all duration-300 group-hover:bg-black group-hover:border-black group-hover:text-white text-gray-900 place-items-center absolute px-8 py-2 lg:-bottom-4 -bottom-6 -right-6 lg:-right-4 border border-white"}>
+                    <Link passhref={true} href={BOOK_ID}>
+                        <a className={"relative mt-12 mx-auto inline-block transition-all duration-300 group hover:bg-white hover:text-gray-900 overflow-visible xl:px-16 lg:px-14 lg:py-4 md:px-12 md:py-4 px-10 py-3 xl:py-6 text-xl text-white bg-transparent border border-white"}>
+                            Book Now
+                            <span
+                                className={"bg-white grid transition-all duration-300 group-hover:bg-black group-hover:border-black group-hover:text-white text-gray-900 place-items-center absolute px-8 py-2 lg:-bottom-4 -bottom-6 -right-6 lg:-right-4 border border-white"}>
                                             <FaArrowRight/>
                         </span>
-                    </a>
+                        </a>
+                    </Link>
                 </div>
                 <div className={"mt-20"}>
                     <div
-                        className="grid sm:grid-cols-3 grid-cols-2 md:gap-x-10 gap-y-10 gap-x-4 border-b-2 border-gray-200 py-10 px-4">
+                        className="grid sm:grid-cols-3 grid-cols-2 md:gap-x-10 gap-y-10 gap-x-4 py-10 px-4">
                         <div className={"grid"}>
                             <div
                                 className={"text-3xl text-gray-900 bg-gray-50 w-14 h-14 rounded-full grid place-items-center"}>
@@ -44,6 +48,7 @@ export function NewHero() {
                             <button className={"flex py-4 text-sm items-center  hover:border-white text-gray-300"}>
                                 Read More <FaArrowRight/>
                             </button>
+                            <hr className={"w-3/4 border border-gray-200 "}/>
                         </div>
                         <div className={"grid"}>
                             <div
@@ -56,6 +61,7 @@ export function NewHero() {
                             <button className={"flex py-4 text-sm items-center  hover:border-white text-gray-300"}>
                                 Read More <FaArrowRight/>
                             </button>
+                            <hr className={"w-3/4 border border-gray-200 "}/>
                         </div>
                         <div className={"grid"}>
                             <div
@@ -68,6 +74,7 @@ export function NewHero() {
                             <button className={"flex py-4 text-sm items-center  hover:border-white text-gray-300"}>
                                 Read More <FaArrowRight/>
                             </button>
+                            <hr className={"w-3/4 border border-gray-200 "}/>
                         </div>
 
                     </div>
