0e58f7c6de3c84b0650113fa410434764355740b
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-03-12 03:08:29 +0600

    Button Style Change Z-Index Update
    Signed-off-by: Khan Asfi Reza <khanasfireza10@gmail.com>

--- components/Button.js
+++ components/Button.js
@@ -3,7 +3,7 @@ import {FaArrowAltCircleUp} from "react-icons/fa";
 export function ToTopButton({className, href}) {
     return (
         <a href={href}
-           className={"fixed bottom-16 right-2 text-3xl  bg-sky-500 text-white text-center rounded-full p-2 transition-all " + className}>
+           className={"fixed z-[10000] bottom-16 right-2 text-3xl  bg-white text-black text-center rounded-full p-2 transition-all " + className}>
             <FaArrowAltCircleUp/>
         </a>
     )
